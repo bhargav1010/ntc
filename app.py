3c3d00d0e9a07f77a9da75f5ede862be0318aa87
Bhargav <66180323+bhargav1010@users.noreply.github.com>
2022-03-17 14:25:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ bert_preprocess = hub.load("https://tfhub.dev/tensorflow/bert_en_uncased_preproc
 bert_encoder = hub.load("https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/4")
 
 le_name_mapping={0: 'BUSINESS', 1: 'EDUCATION', 2: 'ENTERTAINMENT', 3: 'FOOD & DRINK', 4: 'POLITICS', 5: 'SPORTS', 6: 'TECH', 7: 'WELLNESS'}
-
+@app.route('/predict',methods=['POST'])
 def predict():
     text= input("Enter The Data", type=TEXT)
     #text = textarea('Text Area', rows=3, placeholder='Some text')
@@ -36,27 +36,28 @@ def predict():
     vectors_=bert_encoder(bp_)['pooled_output']
     vec=ss.transform(vectors_)
     prediction=ntc_model.predict(vec)
-    put_text('prediction = %r' % le_name_mapping[prediction[0]])
+    return put_text('prediction = %r' % le_name_mapping[prediction[0]])
 #app.add_url_rule('/ntc','webio_view',webio_view(predict),methods=['GET','POST','OPTIONS'])
-'''
+
 from threading import Thread
 from testbot import test 
 
 Thread(target=predict()).start()
 app.run(debug=True,host='0.0.0.0')
-'''
+
 
 '''import os
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)'''
 
-if __name__ == '__main__':
+'''if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=int, default=8080)
     args, unknown = parser.parse_known_args()
     start_server(predict, port=args.port)
+    '''
 #if __name__ == '__main__':
 #   app.run(debug=True)
 
